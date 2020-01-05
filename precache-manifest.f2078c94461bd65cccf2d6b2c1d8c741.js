b0ba9e92aada9728611eee850b2fe79ffc78d140
balachandar <balachandark.bsf.io>
2020-01-05 21:10:48 +0530

    Updates

---
+++ precache-manifest.f2078c94461bd65cccf2d6b2c1d8c741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9aeae52dcf7cc246c844526c5978eec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "789f8dfc6c836f4bf0ed",
+    "url": "/monsters-rolodex/static/css/main.5b2ab9c4.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "789f8dfc6c836f4bf0ed",
+    "url": "/monsters-rolodex/static/js/main.b296828b.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
