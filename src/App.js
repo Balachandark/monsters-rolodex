de3409d882f222821599be8056f486ee71beed44
balachandar <balachandark.bsf.io>
2020-01-05 21:04:57 +0530

    created a monsters rolodex app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      title: 'Monsters Rolodex',
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch( 'https://jsonplaceholder.typicode.com/users' )
+    .then( response => response.json() )
+    .then( users => 
+      this.setState(
+        { monsters: users }
+      )
+    )
+  }
+
+  ChangeHandler = (e) => {
+    this.setState({
+      searchField: e.target.value
+    })
+  }
+  
+  render() {
+    const { monsters, title: mainTitle, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1 className="monsters-title">{mainTitle}</h1>
+        <div className="search-wrapper">
+          <input type="search" className="search" onChange={this.ChangeHandler} placeholder="Search Monsters..." />
+        </div>
+        <div className="monsters-wrapper">
+            {
+              filteredMonsters.map( users => {
+                return(
+                  <div className="monsters" key={users.username}>
+                    <img
+                      alt='monster'
+                      src={`https://robohash.org/${users.id}?set=set2&size=180x180`}
+                    />
+                    <h2>{ users.name }</h2>
+                    <p>{ users.email }</p>
+                  </div>
+                )
+              } )
+            }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
